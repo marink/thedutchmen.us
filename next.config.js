26f6ca2b84f6f95ccd2a0245f067d710d397fc86
marinkokona <marin.kokona@theprimacy.com>
2022-06-20 16:51:59 -0400

    Changed the logo and build scripts.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    images: {
+        loader: "akamai",
+        path: "/"
+    },
+    reactStrictMode: true,
+    exportPathMap: async function (
+        defaultPathMap,
+        {
+            dev = "false",
+            dir = ".",
+            outDir = "/Users/marin/Projects/thedutchmen.us/dist",
+            distDir = ".",
+            buildId
+        }
+    ) {
+        return {
+            '/': { page: '/' },
+        }
+    },
+
 }
 
 module.exports = nextConfig
