7e6773604cb9d91eea1c22ae6547f6fda989434f
marink <marin.kokona@gmail.com>
2022-09-27 02:15:28 -0400

    Major website makeover

---
+++ pages/_document.js
@@ -0,0 +1,21 @@
+
+
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="./css/w3.css" />
+        <link rel="stylesheet" href="./css/styles.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
