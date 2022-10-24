2412ab462be7d7ac6d5b128b1c513f7ca7a9beef
marink <marin.kokona@gmail.com>
2022-10-24 02:53:51 -0400

    Content adjustments.

---
+++ _next/static/bVTn4I1Kr1BQbF-SOQn42/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/543-6b0aecd070b92c4c.js","static/css/c8ed99887e397193.css","static/chunks/pages/index-6778a7f1e1136de4.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
