b021f4e53df6e56d4e18069b7eac7b6ba73727fc
marink <marin.kokona@gmail.com>
2022-11-13 20:27:34 -0500

    Push code changes

---
+++ _next/static/i0nRh3h9TcRepqxLp9mKw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/543-6b0aecd070b92c4c.js","static/css/c8ed99887e397193.css","static/chunks/pages/index-97a501629cb25b81.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
